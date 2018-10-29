64defd4e14b4b062c5e944647c32cd61d3dcde66
zidane zaffar <zidane.zaffar@gmail.com>
2018-10-29 18:51:10 +0700

    finished feature 3

--- src/main/java/com/apap/tugas1/controller/PegawaiController.java
+++ src/main/java/com/apap/tugas1/controller/PegawaiController.java
@@ -115,6 +115,48 @@ public class PegawaiController {
 		return "tambah-pegawai-sukses";
 	}
 	
+	@RequestMapping(value = "/pegawai/ubah")
+	public String changePegawai(@RequestParam("nip") String nip, Model model) {
+		PegawaiModel pegawai = pegawaiService.findPegawaiByNip(nip);
+		
+		model.addAttribute("listProvinsi", provinsiService.getProvinsiList());
+		model.addAttribute("listJabatan", jabatanService.getAllJabatan());
+		model.addAttribute("pegawai", pegawai);
+		return "ubah-pegawai";	
+	}	
+	
+	@RequestMapping(value = "/pegawai/ubah", method = RequestMethod.POST)
+	private String ubahPegawaiSubmit(@ModelAttribute PegawaiModel pegawai, Model model) {
+		String nip = "";
+		
+		nip += pegawai.getInstansi().getId();
+		//System.out.println(pegawai.getInstansi().getId());
+		//System.out.println(pegawai.getId());
+		
+		String[] tglLahir = pegawai.getTanggalLahir().toString().split("-");
+		String tglLahirString = tglLahir[2] + tglLahir[1] + tglLahir[0].substring(2, 4);
+		nip += tglLahirString;
+		//System.out.println(pegawai.getTanggalLahir());
+		
+		nip += pegawai.getTahunMasuk();
+		//System.out.println(pegawai.getTahunMasuk());
+		
+		int counterSama = 1;
+		for (PegawaiModel pegawaiInstansi:pegawai.getInstansi().getPegawaiInstansi()) {
+			if (pegawaiInstansi.getTahunMasuk().equals(pegawai.getTahunMasuk()) && pegawaiInstansi.getTanggalLahir().equals(pegawai.getTanggalLahir()) && pegawaiInstansi.getId() != pegawai.getId()) {
+				counterSama += 1;
+			}	
+		}
+		nip += "0" + counterSama;
+
+		pegawai.setNip(nip);
+		//System.out.println(pegawai.getNip());
+		//System.out.println(pegawai.getId());
+		pegawaiService.addPegawai(pegawai);
+		model.addAttribute("pegawai", pegawai);
+		return "ubah-pegawai-sukses";
+	}
+	
 	
 	@RequestMapping("/pegawai/termuda-tertua")
 	private String viewTermudaTertua(@RequestParam("instansi") long id, Model model) {
--- src/main/java/com/apap/tugas1/service/PegawaiServiceImpl.java
+++ src/main/java/com/apap/tugas1/service/PegawaiServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.apap.tugas1.model.JabatanModel;
 import com.apap.tugas1.model.PegawaiModel;
 import com.apap.tugas1.repository.PegawaiDb;
 
